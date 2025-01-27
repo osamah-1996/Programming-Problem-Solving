48dea79ab80858c2f53712b15ddd8c8a888bfe4b
osamah-1996 <osamah.2018.1996@gmail.com>
2025-01-27 21:23:40 +0300

    Number Input and Output

---
+++ Number Input and Output/Number Input and Output/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Number_Input_and_Output
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Write a program that allows the user to enter a number then print it.
+            int userInput;
+            Console.Write("Please Enter a Number :");
+            int.TryParse(Console.ReadLine(), out userInput);
+            Console.WriteLine($"The Number You have Entered is {userInput}");
+            #endregion
+
+
+
+
+        }
+    }
+}
