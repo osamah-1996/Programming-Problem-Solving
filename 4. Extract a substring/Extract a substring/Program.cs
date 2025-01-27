cce39153f4353e951d8e35fe8a2843e4b1f1fabc
osamah-1996 <osamah.2018.1996@gmail.com>
2025-01-27 22:30:01 +0300

    Extract a substring from String

---
+++ 4. Extract a substring/Extract a substring/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Extract_a_substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Write C# program that Extract a substring from a given string.
+            string? userInput;
+            Console.Write("Please Enter any String :");
+             userInput = Console.ReadLine();
+            while (userInput is null)
+            {
+                Console.Write("Please Enter any String :");
+                userInput = Console.ReadLine();
+            }
+            Console.WriteLine($"The Full Input String is {userInput}");
+            Console.WriteLine($"The Sustring Input  is {userInput.Substring(0, 4)}");
+            #endregion
+        }
+    }
+}
---
+++ Value Type Assignment and Independence/Value Type Assignment and Independence/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Value_Type_Assignment_and_Independence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
