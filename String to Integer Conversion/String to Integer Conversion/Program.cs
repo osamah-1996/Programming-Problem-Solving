1e7b8041f571993cdf16924bb6074976ca138b06
osamah-1996 <osamah.2018.1996@gmail.com>
2025-01-27 21:33:09 +0300

    String to integer Converstion

---
+++ String to Integer Conversion/String to Integer Conversion/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace String_to_Integer_Conversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Write C# program that converts a string to an integer, but the string contains non-numeric characters. And mention what will happen
+
+            Console.Write("Please Enter a Converable  String :");
+            string userInput = Console.ReadLine() ?? "0";
+
+            try{
+                int myString = int.Parse(userInput);
+                Console.WriteLine($"You have Entered {myString}");
+            }
+            catch(FormatException) {
+                Console.WriteLine($"You have a format Excpation Error");
+
+            }
+
+            #endregion
+        }
+    }
+}
