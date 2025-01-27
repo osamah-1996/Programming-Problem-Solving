3897b1c94ff8d59d0a357e909fc129c3b3d74a25
osamah-1996 <osamah.2018.1996@gmail.com>
2025-01-27 22:16:31 +0300

    Simple Arithmetic with floatting

---
+++ 3. Simple Arithmetic with Floating-Point Numbers/imple Arithmetic with Floating-Point Numbers/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace imple_Arithmetic_with_Floating_Point_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Write C# program that Perform a simple arithmetic operation with floating-point numbers And mention what will happen
+
+            bool isPareced;
+            float fNumber;
+            string charr;
+            float SNumber;
+
+
+            do
+            {
+                Console.Write("Please Enter the First Number:");
+                isPareced = float.TryParse(Console.ReadLine(), out  fNumber);
+          
+            } while (!isPareced); 
+            
+            do
+            {
+                Console.Write("Please Enter the Second Number:");
+                isPareced = float.TryParse(Console.ReadLine(), out  SNumber);
+
+
+
+            } while (!isPareced); 
+            
+             
+                Console.Write("Please Enter the Operation (+ - / *):");
+                 charr = Console.ReadLine();
+
+
+            float addiation = fNumber + SNumber;
+            float subtraction = fNumber - SNumber;
+            float multiplication = fNumber * SNumber;
+            float diviion = fNumber / SNumber;
+            switch (charr)
+            {
+                
+                case "+":
+                    Console.WriteLine(string.Format("{0}  + {1}  = {2}", fNumber, SNumber, addiation));
+                    break; 
+                case "-":
+                    Console.WriteLine(string.Format("{0}  - {1}  = {2}", fNumber, SNumber, subtraction));
+                    break; 
+                case "*":
+                    Console.WriteLine(string.Format("{0}  X {1}  = {2}", fNumber, SNumber, multiplication));
+                    break;
+                case "/":
+                    Console.WriteLine(string.Format("{0}  /  {1}  = {2}", fNumber, SNumber, diviion));
+                    break;
+            }
+
+
+            #endregion
+        }
+    }
+}
---
+++ Extract a substring/Extract a substring/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Extract_a_substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
