e0d6a90138f0d706e59c92de9868c04d8f86acca
osamah-1996 <osamah.2018.1996@gmail.com>
2025-01-27 23:55:56 +0300

    Assign Value Data Type to another

---
+++ 5. Value Type Assignment and Independence/Value Type Assignment and Independence/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Value_Type_Assignment_and_Independence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Write C# program that Assigning one value type variable to another and modifying the value of one variable and mention what will happen
+            bool isParced;
+            int userValueType;
+            try
+            {
+                do
+                {
+                    Console.Write("Please a Value Type number :");
+                    isParced = int.TryParse(Console.ReadLine(), out  userValueType); 
+                }
+                while (!isParced);
+
+                Console.WriteLine($"The Value you Have Entered is {userValueType}");
+                userValueType = 100;
+                Console.WriteLine($"The Value you Have Entered after reassigning is {userValueType}");
+
+            }
+            catch (Exception ex) 
+            {
+             Console.WriteLine($"the {ex.ToString()} Excepation is Accure.");
+            }
+             #endregion
+        }
+    }
+}
